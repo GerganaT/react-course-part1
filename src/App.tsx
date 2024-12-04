a3613b3d55943336290f18ad0e2d2fa7033d3bf4
Gergana Toshkova <gerganat@anvilo.com>
2024-12-04 19:37:34 +0200

    Fixed formatting of App component code

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,7 @@ function App() {
 
   // const [selectedCategory, setSelectedCategory] = useState("");
   // const [expenses, setExpenses] = useState([
-  //   {
-  //     id: 1,
-  //     description: "aaa",
-  //     amount: 10,
-  //     category: "Utilities",
-  //   },
+  //   { id: 1, description: "bbb", amount: 10, category: "Utilities" },
   //   { id: 2, description: "bbb", amount: 10, category: "Utilities" },
   //   { id: 3, description: "ccc", amount: 10, category: "Utilities" },
   //   { id: 4, description: "ddd", amount: 10, category: "Utilities" },
